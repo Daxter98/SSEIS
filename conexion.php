91091898bc823afe8ae5e510645f3add570e48bc
MoS09 <cmonroys1500@alumno.ipn.mx>
2022-12-16 00:53:55 -0600

    Cambio host

--- conexion.php
+++ conexion.php
@@ -6,12 +6,12 @@ error_reporting(E_ALL ^ E_NOTICE);
 $conexion = mysqli_init(); 
 $usuario ="sseis_root";
 $contrasena ="DevTest@01";  
-$host = "sseis.mysql.database.azure.com";
+$host = "sseis-server.mysql.database.azure.com";
 $base = "sseis";
 
 
 mysqli_ssl_set($conexion,NULL,NULL, "certificados/DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
-mysqli_real_connect($conexion, "sseis.mysql.database.azure.com", "sseis_root", "DevTest@01", "sseis", 3306, MYSQLI_CLIENT_SSL) or die ("Sin conexion a la B.D");
+mysqli_real_connect($conexion, "sseis-server.mysql.database.azure.com", "sseis_root", "DevTest@01", "sseis", 3306, MYSQLI_CLIENT_SSL) or die ("Sin conexion a la B.D");
 
 /*
 
