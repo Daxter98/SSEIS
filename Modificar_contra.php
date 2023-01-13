aeb185c4f824d1d813d87030709842dc82d700e4
hAlexis1652 <halexis1652@gmail.com>
2023-01-12 23:51:12 -0600

    bueno

--- Modificar_contra.php
+++ Modificar_contra.php
@@ -1,10 +1,5 @@
 <?php 
 
-session_start();
-if(isset($_SESSION['hola']) && $_SESSION ['hola'] == FALSE ) {
-    header ("Location: index.html");
-}
-include "fuctions.php";
 //conexion con servidor y db
 //nombre de la base de datos para seleccionar la base
 //$basededatos="sseis";
