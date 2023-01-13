dabc4ecb72d2f7ecc88deef1037389ddee35cb27
hAlexis1652 <halexis1652@gmail.com>
2023-01-12 23:32:24 -0600

    contra

--- Modificar_contra.php
+++ Modificar_contra.php
@@ -4,6 +4,8 @@
 //$basededatos="sseis";
 //$conexion = mysqli_connect('localhost', $usuario, $contrasena, $base) or die ("Sin conexion :(");
 //valores obtenidos del archivo formulario
+$basededatos = "sseis";
+
 
 
 
@@ -39,3 +41,81 @@
 
 
         ?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <!-- Viewport -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon -->
+    <link rel="icon" type="favicon/x-icon" href="img/logos/IPN.png" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/main.css">
+    <!-- Iconos de Font Awesome -->
+    <link rel="stylesheet" href="css/all.min.css">
+    <!-- Styles SweetAlert -->
+    <link rel="stylesheet" href="css/sweetalert2.min.css">
+    <title>Administrador || Control SSEIS</title>
+</head>
+<body>
+    <div class="container p-4">
+        
+		<div class="row">
+            <div class="col d-flex justify-content-around align-items-center">
+                <img class="img-fluid" width="90px" src="img\logos\IPN.png" alt="IPN">
+            </div>
+            <div class="col-9 d-flex justify-content-around align-items-center">
+                <h3 class="text-black mx-5 text-center">CENTRO DE ESTUDIOS CIENTIFICOS Y TECNOLOGICOS NO. 8 "narciso Bassols"</h3>
+            </div>
+            <div class="col d-flex justify-content-around align-items-center">
+                <img class="img-fluid" width="70px" src="img\logos\voca8.png" alt="cecyt 8">
+            </div>
+        </div>
+        <br><br>
+        <div class="row">
+            <h1 class="text-center">CAMBIO CLAVE DE ACCESO</h1>
+            <div class="col-6 col-md-6 d-flex align-items-center"></div>
+            <div  class="fw text-justify">Modifique su contraseña a una cadena de caracteres de 8 a 12 caracteres. Es necesario incluir en su nueva contraseña por lo menos una letra mayúscula, una minúscula, y un número.</div>
+            <div class="form-select-col-11 form-select-sm" aria-label=".form-select-sm example"></div>
+        
+            <br><br>
+            <form action="" method="post">
+
+            
+
+            <center>
+                <table>
+                    <tr>
+                    <label>Contraseña actual:  <input type="password" name="passActual" autocomplete="off">
+                    </tr>
+                    <p> </p>
+                    <tr>
+                     <label>Contraseña nueva:  <input type="password" name="pass1" autocomplete="off">
+                    </tr>
+                    <p> </p>
+                    <tr>
+                    <label>Confirmar contraseña:  <input type="password" name="pass2" autocomplete="off">
+                    </tr>
+                    <p> </p>
+
+                    <br><br>
+
+                    <div  class="fw text-center">*IMPORTANTE*
+                        Una vez introducida la contraseña nueva el sistema le cerrará su sesión para que vuelva a ingresar con su nueva contraseña</div>
+                    <div class="form-select-col-11 form-select-sm" aria-label=".form-select-sm example"></div>
+                    <p> </p>
+                     
+                    <tr>
+                     <input type="button"  onclick="validar()" value="Cambiar contraseña" name="editar"> 
+                    </tr>
+                </table>
+         </form>
+
+   
+        </div>
+</body>
+
+</html>
