853ba3bff4f0aadf87cc56ffeb91be2180dbce70
Daxter98 <madrigals.emilio@outlook.com>
2023-02-22 01:01:27 -0600

    Se aplican cambios menores en la busqueda de alumn

--- js/alumnos/main.js
+++ js/alumnos/main.js
@@ -13,7 +13,10 @@ $(document).ready(() => {
             success: function(response){
                 let result = JSON.parse(response);
 
+                console.log(result);
                 $("#boleta").html(result.boleta);
+                $("#nombre").html(`${result.nombres} ${result.a_paterno} ${result.a_materno}`);
+                $("#correo-tutor").html(result.correo_tutor);
             },
             error: function(XMLHttpRequest, textStatus, errorThrown){
 
